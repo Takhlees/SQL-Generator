c0445d53a98cae18338b481c2dda97345b0e27ff
Takhlees Fatima <takhleesfatima2003@gmail.com>
2023-07-24 12:14:18 +0500

    Initial commit

---
+++ src/components/MessagesDisplay.tsx
@@ -0,0 +1,22 @@
+import MessageDisplay from "./MessageDisplay"; 
+interface UserMessage {
+  role: string,
+  content: string
+}
+
+interface MessagesDisplayProps {
+   userMessages : UserMessage[]
+}
+
+const MessagesDisplay = ({userMessages} : MessagesDisplayProps) => {
+    return (
+      <div className="messages-display">
+         {userMessages.map((userMessage, index) =>
+          <MessageDisplay key={index} message={userMessage}/>)}
+      
+      </div>
+    );
+  }
+  
+  export default MessagesDisplay;
+
